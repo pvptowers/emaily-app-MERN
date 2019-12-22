77c542c584cb079ca7108243d0412e73daa56d6e
Paul <paultowers2005@yahoo.com.au>
2019-12-22 20:05:31 +1100

    add logout user route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -11,8 +11,14 @@ module.exports = app => {
 
   //Router handler for Google callback
   app.get("/auth/google/callback", passport.authenticate("google"));
-
+  //Get Current Logged in user
   app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+  app.get('/api/logout', (req, res) => {
+    //req.logout is available from passport
+    req.logout();
+    //Get back req.user, as they are loggedout it should come back undefined or empty.
     res.send(req.user);
   });
 };

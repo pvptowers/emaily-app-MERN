215c62f450b275c8acca453b20ab3ce733945a3b
Paul <paultowers2005@yahoo.com.au>
2019-12-22 20:02:23 +1100

    get current logged in user route to test authentication

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -11,4 +11,8 @@ module.exports = app => {
 
   //Router handler for Google callback
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };

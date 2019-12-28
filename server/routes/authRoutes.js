e916d30412d2b41adb3e5e986b228a5020e1f10a
Paul <paultowers2005@yahoo.com.au>
2019-12-28 21:43:02 +1100

    add URL redirect on login

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -10,12 +10,18 @@ module.exports = app => {
   );
 
   //Router handler for Google callback
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
   //Get Current Logged in user
-  app.get('/api/current_user', (req, res) => {
+  app.get("/api/current_user", (req, res) => {
     res.send(req.user);
   });
-  app.get('/api/logout', (req, res) => {
+  app.get("/api/logout", (req, res) => {
     //req.logout is available from passport
     req.logout();
     //Get back req.user, as they are loggedout it should come back undefined or empty.

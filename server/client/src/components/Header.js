7499db245a4ef5948d31efd357b9b25eca0e06d8
Paul <paultowers2005@yahoo.com.au>
2019-12-28 22:42:51 +1100

    created Payments component

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
   //Have header component display differently based on whether user is logged in or not
